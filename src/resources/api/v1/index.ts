1126aa69aa65f3385b49f59d014f2ba1d3b6ee65
frankhn <harfrank2@gmail.com>
2020-10-04 08:59:58 +0200

    documentation route config

---
+++ src/resources/api/v1/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from 'swagger/v1';
+
+const router = Router();
+
+router.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default router;
