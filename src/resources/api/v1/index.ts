9284479a4b11ea70d0853c83e17d6a0ca901afcd
frankhn <harfrank2@gmail.com>
2020-10-04 13:55:09 +0200

    adds sneaker route

--- src/resources/api/v1/index.ts
+++ src/resources/api/v1/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import sneakers from './sneakers/sneakers.route'
 import swaggerDocument from 'swagger/v1';
 
 const router = Router();
 
+router.use('/sneakers', sneakers)
 router.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
