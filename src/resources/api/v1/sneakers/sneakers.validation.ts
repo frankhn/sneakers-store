99bd30b2a3b408ba850f2c6eb0aa2549da3db929
frankhn <harfrank2@gmail.com>
2020-10-04 16:48:30 +0200

    adds validation for id param

--- src/resources/api/v1/sneakers/sneakers.validation.ts
+++ src/resources/api/v1/sneakers/sneakers.validation.ts
@@ -4,6 +4,13 @@ import { celebrate, Joi } from 'celebrate'
 // Validates parameters
 export const getMany = celebrate({
     query: Joi.object().keys({
-        page: Joi.number().integer()
+        page: Joi.number().integer().min(1).default(1)
     }),
+})
+
+// Validate id param
+export const getOne = celebrate({
+    params: Joi.object().keys({
+        id: Joi.string().uuid().required()
+    })
 })
